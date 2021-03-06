6fbe0c2ef7be8b6d3cf6864b8c3cc6614d2841eb
inzheng <57610729+inzheng@users.noreply.github.com>
2021-03-06 15:04:06 +0800

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import webapp2
-
-class ShowNumber (webapp2.RequestHandler):
-    def get(self, firstNumber):
-        self.response.write("<html><body><p>%d + %d = %d</p></body></html>" % firstNumber)
-
